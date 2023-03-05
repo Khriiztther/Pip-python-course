533dbd1555f915c9140c0645ab1e9d1bda244a32
Khriiztther <khriiztther@gmail.com>
2023-03-04 20:03:10 -0500

    Downloaded and added cvs charts program to the project

---
+++ app/charts.py
@@ -0,0 +1,21 @@
+import matplotlib.pyplot as plt
+
+def generate_bar(labels, values):
+  fig, ax = plt.subplots()
+  ax.bar(labels, values)
+  plt.savefig('bar.png')
+  plt.close()
+
+def generate_pie(labels, values):
+  fig, ax = plt.subplots()
+  ax.pie(values, labels=labels)
+  ax.axis('equal')
+  plt.savefig('pie.png')
+  plt.close()
+
+if __name__ == '__main__':
+  labels = ['a', 'b', 'c']
+  values = [10, 295, 800]
+  # generate_bar(labels, values)
+  generate_pie(labels, values)
+  
---
+++ app/population_data.py
@@ -0,0 +1,33 @@
+import country_data
+
+import read_csv
+import charts
+
+
+def run():
+
+  readable = read_csv.read_csv('./app/data.csv')
+  # readable = list(filter(lambda x:x['Continent'] == 'South America', readable))
+  # print(readable[0])
+  # selected = input('Enter the country: ')
+  # country = country_data.country_info(readable, selected)
+  # country = country_data.country_info(readable)
+  # print('****' * 10 )
+  # print(country)
+  # print('****' * 10 )
+  # if len(country) > 0:
+  #   country = country[0]
+  #   labels, values = country_data.population_info(country)
+  #   charts.generate_bar(labels, values)
+  countries = list(map(lambda x: x['Country/Territory'], readable))
+  population = list(map(lambda x: x['World Population Percentage'], readable))
+
+  charts.generate_pie(countries, population)
+
+  # for i in readable:
+  #   countries.append(readable[-1])
+  # print(countries)
+
+
+if __name__ == '__main__':
+  run()
